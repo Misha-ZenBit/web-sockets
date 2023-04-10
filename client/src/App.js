cdc301ba978217db394a40ab40bdb0d6392cbb23
Misha Krasnonos <mikhail.k@kitelytech.com>
2023-04-10 17:49:32 +0300

    updatea

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './app.css'
-import LongPulling from "./LongPulling";
-import EventSourcing from "./EventSourcing";
 import WebSock from "./WebSock";
 
 function App() {
